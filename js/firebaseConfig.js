a3a4bd8f6939b1e9d714dede74c5479b73e0ef5a
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2024-11-13 14:30:02 -0500

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,24 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+// Import the functions you need from the SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCEIIp_7mw1lEJi2ySy8rbYI9zIGz1d2d8",
-    authDomain: "nflpool-71337.firebaseapp.com",
-    projectId: "nflpool-71337",
-    storageBucket: "nflpool-71337.appspot.com",
-    messagingSenderId: "2003523098",
-    appId: "1:2003523098:web:713a9905761dabae7863a3",
-    measurementId: "G-1EBF3DPND1",
-    databaseURL: "https://nflpool-71337-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyCEIIp_7mw1lEJi2ySy8rbYI9zIGz1d2d8",
+  authDomain: "nflpool-71337.firebaseapp.com",
+  databaseURL: "https://nflpool-71337-default-rtdb.firebaseio.com",
+  projectId: "nflpool-71337",
+  storageBucket: "nflpool-71337.appspot.com",
+  messagingSenderId: "2003523098",
+  appId: "1:2003523098:web:713a9905761dabae7863a3",
+  measurementId: "G-1EBF3DPND1",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
-const auth = getAuth(app);
-const db = getDatabase(app);
+// Initialize Analytics (optional)
+const analytics = getAnalytics(app);
 
-// Export necessary Firebase services
-export { auth, db, onAuthStateChanged, signInWithEmailAndPassword, ref, set, get, child };
+// Initialize Authentication and Database
+export const auth = getAuth(app);
+export const db = getDatabase(app);
