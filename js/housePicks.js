311517275d578f20475ebd58956d930434591d9c
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2024-11-18 13:24:33 -0500

    Update housePicks.js

--- js/housePicks.js
+++ js/housePicks.js
@@ -83,16 +83,18 @@ function loadHousePicks() {
 
 function getUserName(userId) {
     const userMap = {
-        'fqG1Oo9ZozX2Sa6mipdnYZI4ntb2': 'Luke Romano',
-        '7INNhg6p0gVa3KK5nEmJ811Z4sf1': 'Charles Keegan',
-        'I3RfB1et3bhADFKRQbx3EU6yllI3': 'Ryan Sanders',
+        'fqG1Oo9ZozX2Sa6mipdnYZI4ntb2': '$ Luke Romano',
+        '7INNhg6p0gVa3KK5nEmJ811Z4sf1': '$ Charles Keegan',
+        'I3RfB1et3bhADFKRQbx3EU6yllI3': '$ Ryan Sanders',
         'krvPcOneIcYrzc2GfIHXfsvbrD23': 'William Mathis',
-        '0A2Cs9yZSRSU3iwnTyNQi3MbQdq2': 'Angela Kant',
+        '0A2Cs9yZSRSU3iwnTyNQi3MbQdq2': '$ Angela Kant',
         '67khUuKYmhXxRumUjMpyoDbnq0s2': 'Thomas Romano',
         'JIdq2bYVCZgdAeC0y6P69puNQz43': 'Tony Romano',
         '9PyTK0SHv7YKv7AYw5OV29dwH5q2': 'Emily Rossini',
         'ORxFtuY13VfaUqc2ckcfw084Lxq1': 'Aunt Vicki',
-        'FIKVjOy8P7UTUGqq2WvjkARZPIE2': 'Tommy Kant'
+        'FIKVjOy8P7UTUGqq2WvjkARZPIE2': 'Tommy Kant',
+        'FFIWPuZYzYRI2ibmVbVHDIq1mjj2': '$ De Von',
+        'i6s97ZqeN1YCM39Sjqh65VablvA3': '$ Kyra Kafel'
     };
     return userMap[userId] || userId;
 }
