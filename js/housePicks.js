c95601928c885bfc340dea0af8bf2d89dee5b4b0
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2025-08-07 22:02:16 -0400

    Update housePicks.js

--- js/housePicks.js
+++ js/housePicks.js
@@ -27,8 +27,8 @@ const games = [
 
 
 const gameWinners = {
-    0: '', // Ravens or Colts
-    1: '', // Eagles or Bengals
+    0: 'Ravens', // Ravens or Colts
+    1: 'Eagles', // Eagles or Bengals
     2: '', // Seahawks or Raiders
     3: '', // Falcons or Lions
     4: '', // Panthers or Browns
