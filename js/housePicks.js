0acf8d7cd479acba11a443d459929667d3d48db7
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2025-09-08 16:57:42 -0400

    Update housePicks.js

--- js/housePicks.js
+++ js/housePicks.js
@@ -89,6 +89,7 @@ function getUserName(userId) {
     'gsQAQttBoEOSu4v1qVVqmHxAqsO2': 'Nick Kier',
     'VnBOWzUZh7UAon6NJ6ICX1kVlEE2': 'Connor Moore',
     'pJxZh3lsp9a0MpKVPSHvyIfNTwW2': 'Mel',
+    '154NpbRIlUQyKwyDjsuW2k4J5Io2': 'Brayden Trunnell',
   };
   return userMap[userId] || `User ${userId}`;
 }
