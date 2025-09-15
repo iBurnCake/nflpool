4fe2d40f46fc9f7693baaf97c977361f58e6f3ba
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2025-09-15 18:20:05 -0400

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,12 +1,4 @@
-import {
-  auth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  db,
-  ref,
-  get
-} from './firebaseConfig.js';
+import { auth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, db, ref, get } from './firebaseConfig.js';
 import { showLoader, hideLoader } from './loader.js';
 import { clearBootLoader, setBootMessage } from './boot.js';
 
