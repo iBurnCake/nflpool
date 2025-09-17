6930bcb1ac7df9549832621589f409bc159d1901
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2025-09-16 23:24:42 -0400

    Refactor firebaseConfig.js for better readability

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,20 +1,62 @@
+// js/firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, fetchSignInMethodsForEmail, linkWithCredential} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getDatabase, ref, set, get, child, update, onValue, off } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect as _signInWithRedirect,
+  getRedirectResult as _getRedirectResult,
+  fetchSignInMethodsForEmail,
+  linkWithCredential,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  onValue,
+  off,
+  child,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEIIp_7mw1lEJi2ySy8rbYI9zIGz1d2d8",
-    authDomain: "nflpool-71337.firebaseapp.com",
-    projectId: "nflpool-71337",
-    storageBucket: "nflpool-71337.appspot.com",
-    messagingSenderId: "2003523098",
-    appId: "1:2003523098:web:713a9905761dabae7863a3",
-    measurementId: "G-1EBF3DPND1",
-    databaseURL: "https://nflpool-71337-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyCEIIp_7mw1lEJi2ySy8rbYI9zIGz1d2d8",
+  authDomain: "nflpool-71337.firebaseapp.com",
+  projectId: "nflpool-71337",
+  storageBucket: "nflpool-71337.appspot.com",
+  messagingSenderId: "2003523098",
+  appId: "1:2003523098:web:713a9905761dabae7863a3",
+  measurementId: "G-1EBF3DPND1",
+  databaseURL: "https://nflpool-71337-default-rtdb.firebaseio.com/"
 };
 
-const app = initializeApp(firebaseConfig);
+const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getDatabase(app);
+const db   = getDatabase(app);
 
-export { auth, db, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, fetchSignInMethodsForEmail, linkWithCredential, ref, set, get, child, update, onValue, off };
+// Re-export everything you need (with explicit aliases for clarity)
+export {
+  // Auth
+  auth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  _signInWithRedirect as signInWithRedirect,
+  _getRedirectResult as getRedirectResult,
+  fetchSignInMethodsForEmail,
+  linkWithCredential,
+
+  // RTDB
+  db,
+  ref,
+  set,
+  get,
+  update,
+  onValue,
+  off,
+  child,
+};
