be155fd4735fcd52aba87e4dba66455934d6392e
iBurnCake <75271603+iBurnCake@users.noreply.github.com>
2025-09-16 23:05:59 -0400

    Refactor firebaseConfig.js imports and exports

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,35 +1,20 @@
-// firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import {
-  getAuth, onAuthStateChanged, signInWithEmailAndPassword,
-  GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult,
-  fetchSignInMethodsForEmail, linkWithCredential
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import {
-  getDatabase, ref, set, get, update, onValue, off, child
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, fetchSignInMethodsForEmail, linkWithCredential} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getDatabase, ref, set, get, child, update, onValue, off } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEIIp_7mw1lEJi2ySy8rbYI9zIGz1d2d8",
-  authDomain: "nflpool-71337.firebaseapp.com",
-  projectId: "nflpool-71337",
-  storageBucket: "nflpool-71337.appspot.com",
-  messagingSenderId: "2003523098",
-  appId: "1:2003523098:web:713a9905761dabae7863a3",
-  measurementId: "G-1EBF3DPND1",
-  databaseURL: "https://nflpool-71337-default-rtdb.firebaseio.com/"
+    apiKey: "AIzaSyCEIIp_7mw1lEJi2ySy8rbYI9zIGz1d2d8",
+    authDomain: "nflpool-71337.firebaseapp.com",
+    projectId: "nflpool-71337",
+    storageBucket: "nflpool-71337.appspot.com",
+    messagingSenderId: "2003523098",
+    appId: "1:2003523098:web:713a9905761dabae7863a3",
+    measurementId: "G-1EBF3DPND1",
+    databaseURL: "https://nflpool-71337-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-export {
-  // Auth (including redirect helpers)
-  auth, onAuthStateChanged, signInWithEmailAndPassword,
-  GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult,
-  fetchSignInMethodsForEmail, linkWithCredential,
-
-  // Realtime DB
-  db, ref, set, get, update, onValue, off, child
-};
+export { auth, db, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, fetchSignInMethodsForEmail, linkWithCredential, ref, set, get, child, update, onValue, off };
